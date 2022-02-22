c5169e10fea451035f24af808d17b1f5b224859f
kbmgs <kbmgs00@gmail.com>
2022-02-23 00:08:22 +0800

    20220222-序列化-虚拟存储切片-CombineTextInputFormat案例实操(视频章节91-94)

--- src/main/java/com/atguigu/mapreduce/combineTextInputFormat/WordCountDriver.java
+++ src/main/java/com/atguigu/mapreduce/combineTextInputFormat/WordCountDriver.java
@@ -1,10 +1,11 @@
-package com.atguigu.mapreduce.wordcount;
+package com.atguigu.mapreduce.combineTextInputFormat;
 
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.io.IntWritable;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.lib.input.CombineTextInputFormat;
 import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
 import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
 
@@ -39,10 +40,20 @@ public class WordCountDriver {
         job.setOutputKeyClass(Text.class);
         job.setOutputValueClass(IntWritable.class);
 
+        // 章节3.1.6 CombineTextInputFormat案例实操
+        // 如果不设置InputFormat，它默认用的是TextInputFormat.class
+        job.setInputFormatClass(CombineTextInputFormat.class);
+
+        // 虚拟存储切片最大值设置4m（最终结果为切3片）
+        //CombineTextInputFormat.setMaxInputSplitSize(job, 4194304);
+
+        //虚拟存储切片最大值设置20m
+        CombineTextInputFormat.setMaxInputSplitSize(job, 20971520);
+
         // 6.设置输入路径和输出路径
-        FileInputFormat.setInputPaths(job, new Path("F:\\BaiduNetdiskDownload\\尚硅谷大数据技术之Hadoop3.x\\资料\\资料\\11_input\\inputword"));
+        FileInputFormat.setInputPaths(job, new Path("F:\\BaiduNetdiskDownload\\尚硅谷大数据技术之Hadoop3.x\\资料\\资料\\11_input\\inputcombinetextinputformat"));
         // 在mapreduce中，如果输出路径存在，则程序报错
-        FileOutputFormat.setOutputPath(job, new Path("F:\\BaiduNetdiskDownload\\尚硅谷大数据技术之Hadoop3.x\\资料\\资料\\output0217"));
+        FileOutputFormat.setOutputPath(job, new Path("F:\\BaiduNetdiskDownload\\尚硅谷大数据技术之Hadoop3.x\\资料\\资料\\outputCombine3_0222"));
 
         // 7.提交job
         boolean result = job.waitForCompletion(true);
